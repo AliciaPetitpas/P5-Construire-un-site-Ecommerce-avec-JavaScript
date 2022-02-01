29599426aa4e33b767b50bad5e767a6a018b4bc6
Alicia <alicia.petitpas@gmail.com>
2022-02-01 19:27:56 +0000

    Affichage des produits sur la page d'accueil

---
+++ front/js/product.js
@@ -0,0 +1,26 @@
+let searchParams = new URL(document.location).searchParams;
+let id = searchParams.get("id"); // retrouve les id des produits
+
+console.log(`Produit ${id}`);
+
+const imgAtt = document.querySelector('.item__img');
+const titleAtt = document.getElementById("title");
+const priceAtt = document.getElementById("price");
+const descriptionAtt = document.getElementById("description");
+const colorsAtt = document.querySelector("#colors");
+const quantity = document.querySelector("#quantity")
+
+fetch("http://localhost:3000/api/products/" + id)
+    .then(result => {
+        return result.json();
+    }) // récupère les produits selon leurs id
+
+.then((dataKanap) => {
+        allKanap = dataKanap;
+        imgAtt.setAttribute("src", `${allKanap.imageUrl}`);
+        imgAtt.setAttribute("alt", `${allKanap.altTxt}`);
+        titleAtt.innerHTML = `${allKanap.name}`;
+        priceAtt.innerHTML = `${allKanap.price}`;
+        descriptionAtt.innerHTML = `${allKanap.description}`;
+        colorsAtt.innerHTML += makeColors(allKanap.colors)
+    }) // affiche les données du produit choisi
---
+++ front/js/script.js
@@ -0,0 +1,21 @@
+const container = document.querySelector("#items"); // crée une const container pour l'id items
+
+fetch("http:localhost:3000/api/products")
+    .then(result => {
+        return result.json();
+    }) // récupère les produits et return en objet json
+
+.then((dataKanap) => {
+    const allKanap = dataKanap;
+    console.log(allKanap);
+
+    allKanap.forEach(element => {
+        container.innerHTML += `<a href="./product.html?id=${element._id}">
+        <article>
+          <img src=${element.imageUrl} alt=${element.altTxt}>
+          <h3 class="productName">${element.name}</h3>
+          <p class="productDescription">${element.description}</p>
+        </article>
+      </a> `
+    });
+}); // affiche les produits

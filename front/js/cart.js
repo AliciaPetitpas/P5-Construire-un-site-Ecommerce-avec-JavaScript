a2ed0d267ffb48247f5c8a6cef0a017c5f14a5f1
Alicia <alicia.petitpas@gmail.com>
2022-02-23 15:32:28 +0000

    Validation formulaire du panier

--- front/js/cart.js
+++ front/js/cart.js
@@ -9,6 +9,7 @@ function addItem() {
               <p> Votre panier est vide </p>
             </div>
           `;
+        document.querySelector('.cart__order').style.display = 'none';
     } else {
 
         for (i = 0; i < localCart.length; i++) {
@@ -138,165 +139,196 @@ function deleteItem() {
     }
 }
 
+let formFirstName = document.getElementById("firstName");
+let formLastName = document.getElementById("lastName");
+let formAddress = document.getElementById("address");
+let formCity = document.getElementById("city");
+let formEmail = document.getElementById("email");
+let formOrder = document.getElementById("order");
+
+//VERIFICATION EMAIL
+formEmail.addEventListener('change', function() {
+    validateEmail(this)
+});
+const validateEmail = function(inputMail) {
+    let checkEmail = false;
+    let emailRegExp = new RegExp(/^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/);
+    let testEmail = emailRegExp.test(inputMail.value);
+    if (testEmail) {
+        formEmail.style.boxShadow = '0px 0px 10px green'
+        formEmail.style.boxSizing = 'border-box'
+        document.getElementById("emailErrorMsg").innerHTML = "Email valide !"
+        document.getElementById("emailErrorMsg").style.color = '#A9F570'
+        checkEmail = true;
+    } else {
+        formEmail.style.boxShadow = '0px 0px 10px red'
+        formEmail.style.boxSizing = 'border-box'
+        document.getElementById("emailErrorMsg").innerHTML = `${inputMail.value} n'est pas une adresse mail valide !`
+        document.getElementById("emailErrorMsg").style.color = '#F9BDBD'
+    }
+    return checkEmail;
+};
+
+//CITY
+formCity.addEventListener('change', function() {
+    validateCity(this)
+});
+const validateCity = function(inputCity) {
+    let checkCity = false;
+    let cityRegExp = new RegExp('^[a-zA-Z-9áàâäãåçéèêëíìîïñóòôöõúùûüýÿæœÁÀÂÄÃÅÇÉÈÊËÍÌÎÏÑÓÒÔÖÕÚÙÛÜÝŸÆŒ._\s-]*.{2,}$', 'g');
+    let testCity = cityRegExp.test(inputCity.value)
+    if (testCity) {
+        formCity.style.boxShadow = '0px 0px 10px green'
+        formCity.style.boxSizing = 'border-box'
+        document.getElementById("cityErrorMsg").innerHTML = "Ville valide !"
+        document.getElementById("cityErrorMsg").style.color = '#A9F570'
+        checkCity = true;
+    } else {
+        formCity.style.boxShadow = '0px 0px 10px red'
+        formCity.style.boxSizing = 'border-box'
+        document.getElementById("cityErrorMsg").innerHTML = `${inputCity.value} n'est pas valide !`
+        document.getElementById("cityErrorMsg").style.color = '#F9BDBD'
+    }
+    return checkCity;
+};
+
+//ADDRESS
+formAddress.addEventListener('change', function() {
+    validateAddress(this)
+});
+const validateAddress = function(inputAddress) {
+    let checkAddress = false;
+    let addressRegExp = new RegExp("^[0-9]{1,3}(?:(?:[,. ]){1}[-a-zA-Zàâäéèêëïîôöùûüç]+)+");
+    let testAddress = addressRegExp.test(inputAddress.value)
+    if (testAddress) {
+        formAddress.style.boxShadow = '0px 0px 10px green'
+        formAddress.style.boxSizing = 'border-box'
+        document.getElementById("addressErrorMsg").innerHTML = "Adresse valide !"
+        document.getElementById("addressErrorMsg").style.color = '#A9F570'
+        checkAddress = true;
+    } else {
+        formAddress.style.boxShadow = '0px 0px 10px red'
+        formAddress.style.boxSizing = 'border-box'
+        document.getElementById("addressErrorMsg").innerHTML = `${inputAddress.value} n'est pas valide !`
+        document.getElementById("addressErrorMsg").style.color = '#F9BDBD'
+    }
+    return checkAddress;
+};
+
+//LASTNAME
+formLastName.addEventListener('change', function() {
+    validateLastName(this)
+});
+const validateLastName = function(inputLastName) {
+    let checkLastName = false;
+    let lastNameRegExp = new RegExp('^[a-zA-ZáàâäãåçéèêëíìîïñóòôöõúùûüýÿæœÁÀÂÄÃÅÇÉÈÊËÍÌÎÏÑÓÒÔÖÕÚÙÛÜÝŸÆŒ._\s -]*.{2,}$', 'g');
+    let testLastName = lastNameRegExp.test(inputLastName.value)
+    if (testLastName) {
+        formLastName.style.boxShadow = '0px 0px 10px green'
+        formLastName.style.boxSizing = 'border-box'
+        document.getElementById("lastNameErrorMsg").innerHTML = "Nom valide !"
+        document.getElementById("lastNameErrorMsg").style.color = '#A9F570'
+        checkLastName = true;
+    } else {
+        formLastName.style.boxShadow = '0px 0px 10px red'
+        formLastName.style.boxSizing = 'border-box'
+        document.getElementById("lastNameErrorMsg").innerHTML = `${inputLastName.value} n'est pas valide !`
+        document.getElementById("lastNameErrorMsg").style.color = '#F9BDBD'
+    }
+    return checkLastName;
+};
+
+//FIRSTNAME
+formFirstName.addEventListener('change', function() {
+    validateFirstName(this)
+});
+const validateFirstName = function(inputFirstName) {
+    checkFirstName = false;
+    let firstNameRegExp = new RegExp('^[a-zA-Z-9áàâäãåçéèêëíìîïñóòôöõúùûüýÿæœÁÀÂÄÃÅÇÉÈÊËÍÌÎÏÑÓÒÔÖÕÚÙÛÜÝŸÆŒ._\s -]*.{2,}$', 'g');
+    let testFirstName = firstNameRegExp.test(inputFirstName.value)
+    if (testFirstName) {
+        formFirstName.style.boxShadow = '0px 0px 10px green'
+        formFirstName.style.boxSizing = 'border-box'
+        document.getElementById("firstNameErrorMsg").innerHTML = "Prénom valide !"
+        document.getElementById("firstNameErrorMsg").style.color = '#A9F570'
+        checkFirstName = true;
+    } else {
+        formFirstName.style.boxShadow = '0px 0px 10px red'
+        formFirstName.style.boxSizing = 'border-box'
+        document.getElementById("firstNameErrorMsg").innerHTML = `${inputFirstName.value} n'est pas valide !`
+        document.getElementById("firstNameErrorMsg").style.color = '#F9BDBD'
+    }
+    return checkFirstName;
+};
+
 function checkForm() {
-    let formFirstName = document.getElementById("firstName");
-    let formLastName = document.getElementById("lastName");
-    let formAddress = document.getElementById("address");
-    let formCity = document.getElementById("city");
-    let formEmail = document.getElementById("email");
-    let formOrder = document.getElementById("order");
 
-    //EMAIL
-    formEmail.addEventListener('change', function(event) {
-        validateEmail(this)
-    });
-    const validateEmail = function(inputMail) {
-        let emailRegExp = new RegExp(/^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/);
-        let testEmail = emailRegExp.test(inputMail.value);
-        if (testEmail) {
-            formEmail.style.boxShadow = '0px 0px 10px green'
-            formEmail.style.boxSizing = 'border-box'
-            document.getElementById("emailErrorMsg").innerHTML = "Email valide !"
-            document.getElementById("emailErrorMsg").style.color = '#A9F570'
-        } else {
-            formEmail.style.boxShadow = '0px 0px 10px red'
-            formEmail.style.boxSizing = 'border-box'
-            document.getElementById("emailErrorMsg").innerHTML = `${inputMail.value} n'est pas une adresse mail valide !`
-            document.getElementById("emailErrorMsg").style.color = '#F9BDBD'
-        }
-    };
+    let check = false;
 
-    //CITY
-    formCity.addEventListener('change', function() {
-        validateCity(this)
-    });
-    const validateCity = function(inputCity) {
-        let cityRegExp = new RegExp('^[a-zA-Z-9áàâäãåçéèêëíìîïñóòôöõúùûüýÿæœÁÀÂÄÃÅÇÉÈÊËÍÌÎÏÑÓÒÔÖÕÚÙÛÜÝŸÆŒ._\s-]*.{2,}$', 'g');
-        let testCity = cityRegExp.test(inputCity.value)
-        if (testCity) {
-            formCity.style.boxShadow = '0px 0px 10px green'
-            formCity.style.boxSizing = 'border-box'
-            document.getElementById("cityErrorMsg").innerHTML = "Ville valide !"
-            document.getElementById("cityErrorMsg").style.color = '#A9F570'
-        } else {
-            formCity.style.boxShadow = '0px 0px 10px red'
-            formCity.style.boxSizing = 'border-box'
-            document.getElementById("cityErrorMsg").innerHTML = `${inputCity.value} n'est pas valide !`
-            document.getElementById("cityErrorMsg").style.color = '#F9BDBD'
-        }
-    };
+    if (validateEmail(formEmail) &&
+        validateAddress(formAddress) &&
+        validateCity(formCity) &&
+        validateFirstName(formFirstName) &&
+        validateLastName(formLastName)) {
+        check = true;
+    }
 
-    //ADDRESS
-    formAddress.addEventListener('change', function() {
-        validateAddress(this)
-    });
-    const validateAddress = function(inputAddress) {
-        let addressRegExp = new RegExp("^[0-9]{1,3}(?:(?:[,. ]){1}[-a-zA-Zàâäéèêëïîôöùûüç]+)+");
-        let testAddress = addressRegExp.test(inputAddress.value)
-        if (testAddress) {
-            formAddress.style.boxShadow = '0px 0px 10px green'
-            formAddress.style.boxSizing = 'border-box'
-            document.getElementById("addressErrorMsg").innerHTML = "Adresse valide !"
-            document.getElementById("addressErrorMsg").style.color = '#A9F570'
-        } else {
-            formAddress.style.boxShadow = '0px 0px 10px red'
-            formAddress.style.boxSizing = 'border-box'
-            document.getElementById("addressErrorMsg").innerHTML = `${inputAddress.value} n'est pas valide !`
-            document.getElementById("addressErrorMsg").style.color = '#F9BDBD'
-        }
-    };
-
-    //LASTNAME
-    formLastName.addEventListener('change', function() {
-        validateLastName(this)
-    });
-    const validateLastName = function(inputLastName) {
-        let lastNameRegExp = new RegExp('^[a-zA-ZáàâäãåçéèêëíìîïñóòôöõúùûüýÿæœÁÀÂÄÃÅÇÉÈÊËÍÌÎÏÑÓÒÔÖÕÚÙÛÜÝŸÆŒ._\s -]*.{2,}$', 'g');
-        let testLastName = lastNameRegExp.test(inputLastName.value)
-        if (testLastName) {
-            formLastName.style.boxShadow = '0px 0px 10px green'
-            formLastName.style.boxSizing = 'border-box'
-            document.getElementById("lastNameErrorMsg").innerHTML = "Nom valide !"
-            document.getElementById("lastNameErrorMsg").style.color = '#A9F570'
-        } else {
-            formLastName.style.boxShadow = '0px 0px 10px red'
-            formLastName.style.boxSizing = 'border-box'
-            document.getElementById("lastNameErrorMsg").innerHTML = `${inputLastName.value} n'est pas valide !`
-            document.getElementById("lastNameErrorMsg").style.color = '#F9BDBD'
-        }
-    };
-
-    //FIRSTNAME
-    formFirstName.addEventListener('change', function() {
-        validateFirstName(this)
-    });
-    const validateFirstName = function(inputFirstName) {
-        let firstNameRegExp = new RegExp('^[a-zA-Z-9áàâäãåçéèêëíìîïñóòôöõúùûüýÿæœÁÀÂÄÃÅÇÉÈÊËÍÌÎÏÑÓÒÔÖÕÚÙÛÜÝŸÆŒ._\s -]*.{2,}$', 'g');
-        let testFirstName = firstNameRegExp.test(inputFirstName.value)
-        if (testFirstName) {
-            formFirstName.style.boxShadow = '0px 0px 10px green'
-            formFirstName.style.boxSizing = 'border-box'
-            document.getElementById("firstNameErrorMsg").innerHTML = "Prénom valide !"
-            document.getElementById("firstNameErrorMsg").style.color = '#A9F570'
-        } else {
-            formFirstName.style.boxShadow = '0px 0px 10px red'
-            formFirstName.style.boxSizing = 'border-box'
-            document.getElementById("firstNameErrorMsg").innerHTML = `${inputFirstName.value} n'est pas valide !`
-            document.getElementById("firstNameErrorMsg").style.color = '#F9BDBD'
-        }
-    };
-
-    //ENVOI FORMULAIRE
-    formOrder.addEventListener('click', function(event) {
-        if (!formFirstName.value ||
-            !formLastName.value ||
-            !formAddress.value ||
-            !formCity.value ||
-            !formEmail.value ||
-            localCart == null
-        ) {
-            event.preventDefault();
-            return alert('Vous ne pouvez pas passer de commande. Veuillez vérifier que votre panier ne soit pas vide et/ou que vos informations ne soient pas erronées.');
-        } else {
-            const contact = {
-                firstName: formFirstName.value,
-                lastName: formLastName.value,
-                address: formAddress.value,
-                city: formCity.value,
-                email: formEmail.value,
-            }
-            localStorage.setItem('Contact', JSON.stringify(contact));
-
-            let products = [];
-            for (m = 0; m < localCart.length; m++) {
-                products.push(localCart[m].id)
-            }
-
-            let submitProducts = { contact, products };
-            console.log(submitProducts);
-
-            fetch("http://localhost:3000/api/products/order", {
-                    method: "POST",
-                    body: JSON.stringify(submitProducts),
-                    headers: {
-                        "content-type": "application/json",
-                    }
-                })
-                .then(res => {
-                    return res.json();
-                }).then((data) => {
-                    let orderId = data.orderId;
-                    window.location.href = `./confirmation.html?id=${orderId}`;
-                    console.log(orderId);
-                }).catch((error) => {
-                    console.log(error);
-                })
-        }
-    });
+    return check;
 }
+
+//ENVOI FORMULAIRE
+formOrder.addEventListener('click', function(event) {
+    event.preventDefault();
+    var valid = true;
+
+    for (let input of document.querySelectorAll(".cart__order__form")) {
+        valid &= input.reportValidity(); //Vérifie la validité des inputs du form
+        if (!valid) {
+            break;
+        }
+    }
+
+    //console.log(valid)
+
+    if (valid == 1 && checkForm()) {
+        //console.log(valid)
+        const contact = {
+            firstName: formFirstName.value,
+            lastName: formLastName.value,
+            address: formAddress.value,
+            city: formCity.value,
+            email: formEmail.value,
+        }
+        localStorage.setItem('Contact', JSON.stringify(contact));
+
+        let products = [];
+        for (m = 0; m < localCart.length; m++) {
+            products.push(localCart[m].id)
+        }
+
+        let submitProducts = { contact, products };
+        console.log(submitProducts);
+
+        fetch("http://localhost:3000/api/products/order", {
+                method: "POST",
+                body: JSON.stringify(submitProducts),
+                headers: {
+                    "content-type": "application/json",
+                }
+            })
+            .then(res => {
+                return res.json();
+            }).then((data) => {
+                let orderId = data.orderId;
+                window.location.href = `./confirmation.html?id=${orderId}`;
+                console.log(orderId);
+            }).catch((error) => {
+                console.log(error);
+            })
+    }
+});
 
 addItem();
 totalPriceProduct();
 modifyCart();
 deleteItem();
-checkForm();
